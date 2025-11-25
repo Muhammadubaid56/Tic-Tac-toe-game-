f54098406a96131ec94c1f2b3fd57d7e8ee5e6da
Muhammad Ubaid <ku83799@gmail.com>
2025-11-25 13:47:40 +0500

    Refactor TicTacToe component to use smaller components and hooks

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+export { default as TicTacToe } from "./TicTacToe";
+export { default as GameBoard } from "./GameBoard";
+export { default as GameSquare } from "./GameSquare";
+export { default as GameModeSelector } from "./GameModeSelector";
+export { default as GameStatus } from "./GameStatus";
+export { default as ResetButton } from "./ResetButton";
+
